5b4494baed7e3d56f4bd0f6d155773c308b1c749
KTNhung <149297429+KTNhung@users.noreply.github.com>
2023-10-29 17:16:02 +0700

    Add files via upload

---
+++ tính toán.py
@@ -0,0 +1,16 @@
+#Chương trình thứ 2
+#tính toán số học
+x=10
+y=5
+tong=x+y
+hieu=x-y
+tich=x*y
+thuong=x/y
+print("Tổng của 2 số", x, "+", y, "=", tong)
+print("Hiệu của 2 số", x, "-", y, "=", hieu)
+print("Tích của 2 số", x, "*", y, "=", tich)
+print("Thương của 2 số", x, "/", y, "=", thuong)
+
+import math
+can_bac_hai_x=math.sqrt(x)
+print("Căn bậc hai của", x,"=", can_bac_hai_x)
