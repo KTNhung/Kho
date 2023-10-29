399643cf221622454c9ca57d37e0910bbbb5cd1c
Khoa hoc du lieu HN 17A2 <trangnhungktn@gmail.com>
2023-10-29 16:47:17 +0700

    First commit

---
+++ tinh_toan_ver_2.py
@@ -0,0 +1,17 @@
+#Chương trình thứ 2
+#tính toán số học
+print("Xin mời nhập số liệu.")
+x=float(input("Nhập giá trị x:"))
+y=float(input("Nhập giá trị y:"))
+tong=x+y
+hieu=x-y
+tich=x*y
+thuong=x/y
+print("Tổng của 2 số", x, "+", y, "=", tong)
+print("Hiệu của 2 số", x, "-", y, "=", hieu)
+print("Tích của 2 số", x, "*", y, "=", tich)
+print("Thương của 2 số", x, "/", y, "=", thuong)
+
+import math
+can_bac_hai_x=math.sqrt(x)
+print("Căn bậc hai của", x,"=", can_bac_hai_x)
