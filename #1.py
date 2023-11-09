ef0e84f81714f9a76f05af64a158d8b939386be7
Khoa hoc du lieu 17A2 <ktnhung.dhkl17a2hn@sv.uneti.edu.vn>
2023-11-09 15:48:56 +0700

    test

---
+++ #1.py
@@ -0,0 +1,18 @@
+def Bai76(): 
+    x = True
+    y = False 
+    print("x and y: ", x and y)
+    print("x or y: ",x or y )
+    print("not x: ", not x)
+    print("x is y: ", x is y)
+    print("x is not y: ", x is not y )
+
+x = 10 
+y = 5
+print("x =",x, "y =",y)
+print("Tổng x + y =", x + y )
+print("Hiệu x - y =", x - y)
+print("Tích x*y = ", x*y )
+print("Thương x/y = ", x/y )
+Bai76()
+
